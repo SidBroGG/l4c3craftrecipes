81b1608c2e2d8d1581b0619eaffddddccffb743f
windows-pc <sasha.sidenko09@gmail.com>
2026-07-12 11:44:31 +0300

    added vitamix craft recipes

--- src/main/java/com/example/l4c3craftrecipes/data/ModRecipeProvider.java
+++ src/main/java/com/example/l4c3craftrecipes/data/ModRecipeProvider.java
@@ -2,6 +2,7 @@ package com.example.l4c3craftrecipes.data;
 
 import com.example.l4c3craftrecipes.L4C3CraftRecipes;
 import com.example.l4c3craftrecipes.registry.ModBlocks;
+import com.example.l4c3craftrecipes.registry.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeCategory;
@@ -42,10 +43,43 @@ public final class ModRecipeProvider extends RecipeProvider {
         );
     }
 
+    private static void recipe_vitamix_multifruit(RecipeOutput output) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.VITAMIX_MULTIFRUIT.value())
+                .pattern("R R")
+                .pattern("RRR")
+                .define('R', ModItems.ROTTEN_FLESH_BLOCK_ITEM.value())
+                .unlockedBy("has_rotten_flesh_block", has(ModItems.ROTTEN_FLESH_BLOCK_ITEM.value()))
+                .save(output);
+    }
+
+    private static void recipe_vitamix_apple_grape(RecipeOutput output) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.VITAMIX_APPLE_GRAPE.value())
+                .pattern("RVR")
+                .pattern("RRR")
+                .define('R', ModItems.ROTTEN_FLESH_BLOCK_ITEM.value())
+                .define('V', ModItems.VITAMIX_MULTIFRUIT.value())
+                .unlockedBy("has_vitamix_multifruit", has(ModItems.VITAMIX_MULTIFRUIT.value()))
+                .save(output);
+    }
+
+    private static void recipe_vitamix_cherry(RecipeOutput output) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.VITAMIX_CHERRY.value())
+                .pattern("R R")
+                .pattern("RVR")
+                .pattern("RRR")
+                .define('R', ModItems.ROTTEN_FLESH_BLOCK_ITEM.value())
+                .define('V', ModItems.VITAMIX_APPLE_GRAPE.value())
+                .unlockedBy("has_vitamix_apple_grape", has(ModItems.VITAMIX_APPLE_GRAPE.value()))
+                .save(output);
+    }
 
     @Override
     protected void buildRecipes(@NotNull RecipeOutput output) {
         recipe_rotten_flesh(output);
         recipe_rotten_flesh_block(output);
+
+        recipe_vitamix_multifruit(output);
+        recipe_vitamix_apple_grape(output);
+        recipe_vitamix_cherry(output);
     }
 }
